d1f9fdb83411e3ebea6a3851964a63f0b7895f46
David Reidsma <david.reidsma@gmail.com>
2022-12-01 10:51:22 -0500

    firebase stuff

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'universalcolorpicker',
+    appId: '1:356716060829:web:a9986072daf305cc0e3474',
+    storageBucket: 'universalcolorpicker.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBBJQ59FiBXUqc2rGg9csgzzHwj3XGUDao',
+    authDomain: 'universalcolorpicker.firebaseapp.com',
+    messagingSenderId: '356716060829',
+  },
   production: false
 };
 
