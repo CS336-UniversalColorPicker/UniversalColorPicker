48fc6b6d6bcd2e640a4d30e32fc6c848e360e217
Ben DeWeerd <bendeweerd2001@gmail.com>
2022-12-01 12:52:59 -0500

    camera work

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CameraPreview, CameraPreviewOptions } from '@capgo/camera-preview';
 import { PhotoService } from '../services/photo.service';
 
 @Component({
@@ -7,11 +8,21 @@ import { PhotoService } from '../services/photo.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  
+
   constructor(public photoService: PhotoService) { }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+
+  takePhoto() {
+    const cameraPreviewOptions: CameraPreviewOptions = {
+      position: 'rear',
+      height: 1920,
+      width: 1080
+    };
+
+    CameraPreview.start(cameraPreviewOptions);
   }
 
 }
